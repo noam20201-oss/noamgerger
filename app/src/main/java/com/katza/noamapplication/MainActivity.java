fe0d08562dd6a4a4958a5b3705ec10db192bf15b
eitannitzani <eithannitzani@gmail.com>
2025-12-10 15:05:01 +0200

    menues

--- app/src/main/java/com/katza/noamapplication/MainActivity.java
+++ app/src/main/java/com/katza/noamapplication/MainActivity.java
@@ -1,14 +1,19 @@
 package com.katza.noamapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.SeekBar;
 import android.widget.Switch;
+import android.widget.TextView;
 import android.widget.Toast;
-
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -18,11 +23,14 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
     Button button1;
     Button button2;
     ImageView imageView;
-    SeekBar seekBar2 ;
+    SeekBar seekBar2;
     Switch switch1;
+    TextView tv; // ← מהתמונה
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -40,26 +48,29 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         button2 = findViewById(R.id.button2);
         switch1 = findViewById(R.id.switch1);
         seekBar2 = findViewById(R.id.seekBar2);
-        
+
+        // ===== מהתמונה =====
+        tv = findViewById(R.id.tv);
+        registerForContextMenu(tv);
+        // ===================
+
         button1.setOnClickListener(this);
         button2.setOnClickListener(this);
+
         seekBar2.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
             public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                // ככל שהסליידר ימינה יותר, התמונה תופיע יותר (שקיפות נמוכה יותר)
-                float alpha = progress / (float) seekBar.getMax(); // הופך את הערך ליחס בין 0 ל-1
+                float alpha = progress / (float) seekBar.getMax();
                 imageView.setAlpha(alpha);
             }
 
             @Override
             public void onStartTrackingTouch(SeekBar seekBar) {
-                // מציג הודעה כשהמשתמש מתחיל להזיז את הסליידר
                 Toast.makeText(MainActivity.this, "התחלת להזיז את הסליידר", Toast.LENGTH_SHORT).show();
             }
 
             @Override
             public void onStopTrackingTouch(SeekBar seekBar) {
-                // מציג הודעה כשהמשתמש מפסיק להזיז את הסליידר
                 int progress = seekBar.getProgress();
                 Toast.makeText(MainActivity.this, "עצרת על ערך: " + progress, Toast.LENGTH_SHORT).show();
             }
@@ -68,25 +79,62 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(@NonNull CompoundButton buttonView, boolean isChecked) {
-                if (isChecked) {
-                    // אם המתג פועל (ON) – הצגת התמונה
-                    imageView.setVisibility(View.VISIBLE);
-                } else {
-                    // אם המתג כבוי (OFF) – הסתרת התמונה
-                    imageView.setVisibility(View.INVISIBLE);
-                }
+                imageView.setVisibility(isChecked ? View.VISIBLE : View.INVISIBLE);
             }
         });
+    }
 
+    // ===== OPTIONS MENU =====
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.activity_main) {
+            Toast.makeText(this, "אתה כבר במסך זה", Toast.LENGTH_SHORT).show();
+            return true;
+        } else if (id == R.id.activity_main2) {
+            Intent intent = new Intent(this, MainActivity2.class);
+            startActivity(intent);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    // ===== CONTEXT MENU =====
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.contextmenu, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+
+        if (item.getItemId() == R.id.firstline) {
+            Toast.makeText(this, "You selected first line", Toast.LENGTH_LONG).show();
+            return true;
+        } else if (item.getItemId() == R.id.secondline) {
+            Toast.makeText(this, "You selected second line", Toast.LENGTH_LONG).show();
+            return true;
+        }
+
+        return super.onContextItemSelected(item);
+    }
+
+    // ===== BUTTONS =====
+    @Override
     public void onClick(View v) {
         if (v.getId() == R.id.button1) {
-            // הצגת התמונה
             imageView.setVisibility(View.VISIBLE);
-        } else if (v.getId() ==R.id.button2) {
-            // הסתרת התמונה
+        } else if (v.getId() == R.id.button2) {
             imageView.setVisibility(View.INVISIBLE);
         }
     }
--- app/src/main/java/com/katza/noamapplication/MainActivity2.java
+++ app/src/main/java/com/katza/noamapplication/MainActivity2.java
@@ -1,24 +1,90 @@
 package com.katza.noamapplication;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.activity.EdgeToEdge;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity2 extends AppCompatActivity {
 
+    Button button;
+    SharedPreferences sp;
+    EditText name;
+    EditText password;
+    TextView dis;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main2);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        sp = getSharedPreferences("noamfile", MODE_PRIVATE);
+
+        button = findViewById(R.id.button);
+        name = findViewById(R.id.name);
+        password = findViewById(R.id.password);
+        dis = findViewById(R.id.dis);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = sp.edit();
+                editor.putString("name1", name.getText().toString());
+                editor.putString("password1", password.getText().toString());
+                editor.commit();
+
+                Toast.makeText(MainActivity2.this, "הנתונים נשמרו", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        String strname = sp.getString("name1", null);
+        String strpassword = sp.getString("password1", null);
+
+        if (strname != null && strpassword != null) {
+            dis.setText("hi " + strname + " your password is " + strpassword);
+        }
+    }
+
+    // ===== MENU =====
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.activity_main) {
+            Intent intent = new Intent(this, MainActivity.class);
+            startActivity(intent);
+            return true;
+        } else if (id == R.id.activity_main2) {
+            Toast.makeText(this, "אתה כבר במסך זה", Toast.LENGTH_SHORT).show();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
